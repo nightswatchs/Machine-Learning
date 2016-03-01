c58279f72fadddd65ea4c31e6983641da4705552
CHENXI <15171427379@163.com>
2016-03-01 17:33:48 +0800

    个数确定完全一致

--- recom1.py
+++ recom1.py
@@ -8,12 +8,15 @@ work = workbook.sheets()[0]
 
 dicts = {}
 data = []
-
+#i = 0
+# 个数确定
 for row in range(7,work.nrows):
     region = work.cell(row,0).value.encode('utf-8')
     dicts[region] = []
+#    i += 1
     for col in range(1, work.ncols):
         dicts[region].append(work.cell(row, col).value)
 
 for region, number in dicts.items():
     print region,number
+#print i
