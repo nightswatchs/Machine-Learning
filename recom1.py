5d4fc370b9bd66c33ebbf0207778f49756349635
CHENXI <15171427379@163.com>
2016-03-02 21:26:10 +0800

    两种方法解释完毕

--- recom1.py
+++ recom1.py
@@ -1,22 +1,14 @@
 #---coding: utf-8---
 import xlrd
+def recom():
+    workbook = xlrd.open_workbook('A0101a.xls')
+    work = workbook.sheets()[0]
+    dicts = {}
 
-#打开文件
-workbook = xlrd.open_workbook('A0101a.xls')
+    for row in range(7,work.nrows):
+        region = work.cell(row,0).value.encode('utf-8')
+        dicts[region] = []
+        for col in range(1, work.ncols):
+            dicts[region].append(work.cell(row, col).value)
+    return dicts
 
-work = workbook.sheets()[0]
-
-dicts = {}
-data = []
-#i = 0
-# 个数确定
-for row in range(7,work.nrows):
-    region = work.cell(row,0).value
-    dicts[region] = []
-#    i += 1
-    for col in range(1, work.ncols):
-        dicts[region].append(work.cell(row, col).value)
-
-#for region, number in dicts.items():
-#    print region,number
-print dicts
