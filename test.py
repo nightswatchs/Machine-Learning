002b138ad89ad5fcf94cd24e61f41dc58a898e15
CHENXI <15171427379@163.com>
2016-03-02 18:47:01 +0800

    使用单元格查找准确率不高，手动吧

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 #---coding: utf-8---
 import xlrd
-work = xlrd.open_workbook('A0101a.xls').sheets()[0]
+work = xlrd.open_workbook('A0101a.xls',formatting_info = True).sheets()[0]
 
 data = []
+#for i in range(1, work.ncols):
+#    data.append(work.cell(6, i).value.encode('utf-8'))
 
-for i in range(1, work.ncols):
-    data.append(work.cell(6, i).value.encode('utf-8'))
-
-for content in data:
-    print content,
+#for content in data:
+#    print content,
+merges = work.merged_cells
+sorts = sorted(merges, key=lambda m:m[0])
+print merges
+print sorts
+for i in sorts:
+    if i[1] == 7:
+        print i
+        data.append(work.cell(i[0],i[2]).value.encode('utf-8'))
+    else:
+        pass
+for j in data:
+    print j
