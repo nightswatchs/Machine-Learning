d8d75286a3a8985d45493fed2ca2d9ac389bc48e
Xing Li <15171427379@163,com>
2016-03-09 20:06:14 +0800

    未完成的del.icio.us,网络问题+未认真哈

---
+++ deliciousrec.py
@@ -0,0 +1,33 @@
+#---coding: utf-8---
+from pydelicious import get_popular,get_userposts,get_urlposts
+
+def initlizeUserDict(tag, count = 5):
+    user_dict = {}
+    #获取所有用户都提交过的链接
+
+    for p1 in get_popular(tag=tag)[0:count]:
+        for p2 in get_urlposts(p1['href']):
+            user = p2['user']
+            user_dict[user]={}
+    return user_dict
+def fillItem(user_dict):
+    all_item = {}
+
+    for user in user_dict:
+        for i in range(3):
+            try:
+                posts = get_urlposts(user)
+                break
+            except:
+                print "Failed user "+user+", retrying"
+                time.sleep(4)
+    for post in posts:
+        url = post['href']
+        user_dict[user][url] = 1.0
+        all_item[url] = 1
+
+    for ratings in user_dict.values:
+        for item in all_item:
+            if item not in ratings:
+                ratings[item] = 0.0
+
